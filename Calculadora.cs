e326d064006a1c6ef0037bfc870846f8c5794365
renata parra <lucasparra2@outlook.com>
2021-03-11 20:12:40 -0300

    Initial commit

---
+++ Calculadora.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Calculadora
+    {
+        // Métodos da Calc;
+
+        public int somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int multi(int num1, int num2)
+        {
+            return num1 / num2;
+
+        }
+        public double div(int num1, int num2)
+        {
+            return num1 * num2;
+
+        }
+    }
+}
